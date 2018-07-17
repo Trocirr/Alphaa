911a1005bc33291c7347de7dcbac8ce241d5e839
Trocirr <38221031+Trocirr@users.noreply.github.com>
2018-07-17 13:09:32 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -135,7 +135,7 @@ async def xdd(ctx):
 	embed.add_field(name=
 	
 	
-@client.command(pass_context=True, description='Shows the info of the mentioned user.')
+@client.command(pass_context=True, description="Shows the info of the mentioned user.")
 async def info(ctx, user: discord.Member):
     embed = discord.Embed(title="{}'s info".format(user.name), description="Here's what I could find:", color=0x00ff00)
     embed.add_field(name="Name", value=user.name, inline=True)
