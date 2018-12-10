b2cf3bf8bcf132c698edfeb907a14fcdf5c2e3e0
Trocirr <38221031+Trocirr@users.noreply.github.com>
2018-12-10 17:55:33 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -65,9 +65,7 @@ async def randomnumber(ctx):
 @commands.has_permissions(kick_members=True)
 async def kick(ctx, userName: discord.User):
     await client.kick(userName)
-    embed = discord.Embed(title="User Kicked!", description="", color=0xff00f6)
-    embed.add_field(name="User Kicked Successfully", color=0xff00f6
-    await client.say(embed=embed)
+    await client.say("User Kicked Successfully")
 
 @client.command(pass_context=True)
 async def purge(ctx, *, amount : int):
