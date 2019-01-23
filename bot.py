019a2d1ff8e2cf6d58618d6d2c75b7ef1408d836
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-23 22:46:20 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,35 +45,6 @@ async def time(ctx):
 	embed.add_field(name=":stopwatch: **Current Local Time and Date in the United Kingdom**", value=utc)
 	await client.say(embed=embed)	
 	
-@client.command(pass_context=True)
-async def userinfo(ctx, member: discord.Member = None):
-	member = ctx.author if not member else member
-	roles = [role for role in member.roles]
-
-	
-	embed = discord.Embed(colour=member.color, timestamp=ctx.message.created_at)
-
-	
-	embed.set_author(name=f"User Info - {member}")
-	embed.set_thumbnail(url=member.avatar_url)
-	embed.set_footer(text=f"Requested by {ctx.author}", icon_url=ctx.author.avatar_url)
-
-
-	embed.add_field(name="ID:", value=member.id)
-	embed.add_field(name="Guild name:", value=member.display_name)
-
-
-	embed.add_field(name="Created at:" value=member.created_at.strftime("%a, %#d %B %Y, %I: %M %p UTC"))
-	embed.add_field(name="Joined at:", value=member.joined_at.strftime("%a, %#d %B %Y, %I:%M %p UTC"))
-
-
-	embed.add_field(name=f"Roles ({len(roles)})", value=" ".join([role.mention for role in roles]))
-	embed.add_field(name="Top role:", value.top_role.mention)
-
-	embed.add_field(name="Bot?", value=member.bot)
-
-
-	await ctx.send(embed=embed)
 
 @client.command(pass_context = True)
 async def mute(ctx, member: discord.Member):
