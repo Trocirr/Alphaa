07a9246f0b725b05070de2dfceaf0c9762cafb69
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-24 11:34:16 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -71,6 +71,7 @@ async def kick(ctx, userName: discord.User):
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, userName: discord.User):
 	await client.ban(userName)
+	await client.say("User has been banned!")
 
 @client.command(pass_context=True)
 async def purge(ctx, *, amount : int):
