61633f8353007e3f630dc640e85017d96498f9ae
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-24 21:06:32 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -106,6 +106,11 @@ async def exdee(ctx):
     embed.add_field(name="Rules", value="You can talk about anything here as long as it isn't disrespectful. If you have a problem with anyone or anything in this server, please DM the Admins about it. \n \n 1. Rudeness, harassment, and immaturity will not be tolerated. \n 2. Porn and otherwise NSFW/NSFL content is prohibited. \n 3. No discussion about highly illegal activity. \n 4. Copyrighted and other unlawful material is prohibited, such as links to illegal content. \n 5. Advertising and promoting is prohibited. Don't even bother posting invite links because the bots will delete them anyway. \n 6. <#535451148090474497> is our main channel. Keep it clean. \n 7. No (random) links in the general chat <#535451148090474497> unless a moderator or higher lets you!")
     await client.say(embed=embed)
 		
+@client.command(pass_context=True)
+async def div1(ctx):
+    embed = discord.Embed(description="", color=0xFF8C00)
+    embed.add_field(name="1st Division", value="1. Alex28102 :flag_ca: \n 2. Acquah :flag_it: \n Dadem :flag_dz: \n 3. Frozenkiller :flag_nl: \n 4. aalaaprocks :flag_ie: \n Shyrix :flag_fr: \n Trocir :flag_rs:")
+    await client.say(embed=embed)
 	
 @client.command(pass_context=True)
 async def idk(ctx):
