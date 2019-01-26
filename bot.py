cc60b7e199a0785a820c83ddb6be9d6053c32355
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-01-26 16:33:54 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def kick(ctx, userName: discord.User):
 @commands.has_permissions(ban_members=True)
 async def ban(ctx, userName: discord.User):
 	await client.ban(userName)
-	embed=discord.Embed(title="User Banned!", description="<a:Success:468812983074553876> **{0}** has been banned by **{1}**!".format(member, ctx.message.author),  color=0xff00f6)
+	embed=discord.Embed(title="User Banned!", description="<a:Success:468812983074553876> **{0}** has been banned by **{1}**!".format(userName, ctx.message.author),  color=0xff00f6)
 	await client.say(embed=embed)
 
 @client.command(pass_context=True)
