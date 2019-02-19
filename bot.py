ae18cacb7d7824e8e08774dc9fc4baafe59b4e90
Trocirr <38221031+Trocirr@users.noreply.github.com>
2019-02-19 16:33:14 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -136,8 +136,9 @@ async def xd(ctx):
 
 @client.command(pass_context=True)
 async def xdxd(ctx):
-	embed = discord.Embed(title="Jokers F.C.", description="The most elite football club in bonk.io \n Led by elite players Tuccio |C|, Chiilzz and Coldrex \n \n [Link](https://discord.gg/akGqCUH)", color=0xFF7256)
-	embed.set_footer("Partner")
+	embed = discord.Embed(title="Jokers F.C.", description="The most elite football club in bonk.io \n Led by elite players Tuccio |C|, Chiilzz and Coldrex", color=0xFF7256)
+	embed.add_field(name="Invite to this Server", value="https://discord.gg/akGqCUH")
+	embed.set_footer(text="Partner")
 	await client.say(embed=embed)
 
 @client.command(pass_context=True)
